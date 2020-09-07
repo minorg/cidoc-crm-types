c0f29d2cdeb1e9593a093175d1fc965621b865c1
Minor G <minorg@server.fake>
2020-09-06 20:43:10 -0400

    parse properties

--- generator/cidoc_crm_types_generator/ecrm_owl_parser.py
+++ generator/cidoc_crm_types_generator/ecrm_owl_parser.py
@@ -5,6 +5,7 @@ from rdflib import BNode, Graph, OWL, RDF, RDFS, SKOS, URIRef
 
 from cidoc_crm_types_generator.ecrm_owl import EcrmOwl
 from cidoc_crm_types_generator.entity_class import EntityClass
+from cidoc_crm_types_generator.property import Property
 from cidoc_crm_types_generator.property_restriction import PropertyRestriction
 
 
@@ -18,7 +19,8 @@ class EcrmOwlParser:
 
     def parse(self, graph: Graph) -> EcrmOwl:
         entity_classes = self.__parse_entity_classes(graph)
-        return EcrmOwl(entity_classes=entity_classes, properties=())
+        properties = self.__parse_properties(graph)
+        return EcrmOwl(entity_classes=entity_classes, properties=properties)
 
     def __parse_entity_class(self, *, entity_class_uri: URIRef, graph: Graph):
         comment = None
@@ -79,18 +81,78 @@ class EcrmOwlParser:
         )
 
     def __parse_entity_classes(self, graph: Graph) -> Tuple[EntityClass, ...]:
-        entity_classes = []
-        for entity_class_uri in graph.subjects(RDF.type, OWL.Class):
-            entity_classes.append(
-                self.__parse_entity_class(
-                    graph=graph, entity_class_uri=entity_class_uri
+        return tuple(
+            self.__parse_entity_class(graph=graph, entity_class_uri=entity_class_uri)
+            for entity_class_uri in graph.subjects(RDF.type, OWL.Class)
+        )
+
+    def __parse_properties(self, graph: Graph) -> Tuple[Property, ...]:
+        return tuple(
+            self.__parse_property(graph=graph, property_uri=property_uri)
+            for property_uri in graph.subjects(RDF.type, OWL.ObjectProperty)
+        )
+
+    def __parse_property(self, *, graph: Graph, property_uri: URIRef) -> Property:
+        comment = None
+        domain = None
+        inverse_of = None
+        label = None
+        notation = None
+        range = None
+        sub_property_of = []
+        for property_predicate, property_object in graph.predicate_objects(
+            property_uri
+        ):
+            if property_predicate == OWL.inverseOf:
+                assert inverse_of is None
+                inverse_of = property_object
+                assert isinstance(inverse_of, URIRef)
+            elif property_predicate == RDF.type:
+                continue
+            elif property_predicate == RDFS.comment:
+                assert comment is None, property_uri
+                comment = property_object.value
+                assert isinstance(comment, str)
+            elif property_predicate == RDFS.domain:
+                assert domain is None, property_uri
+                domain = property_object
+                assert isinstance(domain, URIRef)
+            elif property_predicate == RDFS.label:
+                assert label is None, property_uri
+                label = property_object.value
+                assert isinstance(label, str)
+            elif property_predicate == RDFS.range:
+                assert range is None, property_uri
+                range = property_object
+                assert isinstance(range, URIRef)
+            elif property_predicate == RDFS.subPropertyOf:
+                sub_property_of.append(property_object)
+                assert isinstance(sub_property_of[-1], URIRef)
+            elif property_predicate == SKOS.notation:
+                assert notation is None, property_uri
+                notation = property_object.value
+            else:
+                self.__logger.warning(
+                    "unhandled property (%s) predicate: %s, %s (%s)",
+                    property_uri,
+                    property_predicate,
+                    property_object,
+                    type(property_object),
                 )
-            )
-        return tuple(entity_classes)
+        return Property(
+            comment=comment,
+            domain=domain,
+            inverse_of=inverse_of,
+            label=label,
+            notation=notation,
+            range=range,
+            sub_property_of=tuple(sub_property_of),
+            uri=property_uri,
+        )
 
     def __parse_property_restriction(
         self, *, graph: Graph, property_restriction_uri: URIRef
-    ):
+    ) -> PropertyRestriction:
         cardinality = None
         on_property = None
         max_cardinality = None
--- generator/cidoc_crm_types_generator/property.py
+++ generator/cidoc_crm_types_generator/property.py
@@ -1,7 +1,14 @@
-from typing import NamedTuple
+from typing import NamedTuple, Optional, Tuple
 
 from rdflib import URIRef
 
 
 class Property(NamedTuple):
+    comment: Optional[str]
+    domain: Optional[URIRef]
+    inverse_of: Optional[URIRef]
+    label: Optional[str]
+    notation: Optional[str]
+    range: Optional[URIRef]
+    sub_property_of: Tuple[URIRef, ...]
     uri: URIRef
