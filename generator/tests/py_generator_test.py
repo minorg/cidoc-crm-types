45a627e89cfcb4b443c509c6f04955f9c764f86d
Minor G <minorg@server.fake>
2020-09-07 15:17:11 -0400

    fix py generator test

--- generator/tests/py_generator_test.py
+++ generator/tests/py_generator_test.py
@@ -5,4 +5,4 @@ from cidoc_crm_types_generator.py_generator import PyGenerator
 
 
 def test_generate(ecrm_owl: EcrmOwl, tmpdir):
-    PyGenerator(output_dir_path=Path(tmpdir)).generate(ecrm_owl)
+    PyGenerator(output_dir_path=Path(tmpdir)).generate(ecrm_owl=ecrm_owl)
